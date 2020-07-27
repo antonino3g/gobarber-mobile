40772cf7c5468af33b26bf5b08f2e0550902a6a7
Antonino Praxedes <antonino.praxedes@gmail.com>
2020-07-26 22:20:03 -0400

    feat: connection signup with api

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -56,9 +57,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você já pode fazer login na aplicação',
+      );
 
-      // history.push('/');
+      await api.post('/users', data);
+
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -69,7 +75,7 @@ const SignUp: React.FC = () => {
       }
       Alert.alert(
         'Erro no cadastro',
-        'Ocorreu um erro ao fazer cadastro, tente novamente',
+        'Ocorreu um erro ao fazer o cadastro, tente novamente',
       );
     }
   }, []);
